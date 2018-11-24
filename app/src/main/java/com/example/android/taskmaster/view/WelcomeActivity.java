6aa3022ce6158f0dfd1bbf29076066aab61083b1
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-11-24 15:50:31 -0600

    Implemented click handlers for the log in and sign up buttons.

--- app/src/main/java/com/example/android/taskmaster/view/WelcomeActivity.java
+++ app/src/main/java/com/example/android/taskmaster/view/WelcomeActivity.java
@@ -1,16 +1,53 @@
 package com.example.android.taskmaster.view;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.android.taskmaster.R;
+import com.example.android.taskmaster.databinding.ActivityWelcomeBinding;
 
 public class WelcomeActivity extends AppCompatActivity
 {
+  private ActivityWelcomeBinding binding;
+
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_welcome);
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome);
+
+    registerClickHandlers();
+  }
+
+  private void registerClickHandlers()
+  {
+    binding.buttonWelcomeActivityLogIn.setOnClickListener(new LogInButtonClickHandler());
+    binding.buttonWelcomeActivitySignUp.setOnClickListener(new SignUpButtonClickHandler());
+  }
+
+  class LogInButtonClickHandler implements View.OnClickListener
+  {
+    @Override
+    public void onClick(View v)
+    {
+      Intent intent = new Intent(WelcomeActivity.this, LogInActivity.class);
+
+      startActivity(intent);
+    }
+  }
+
+  class SignUpButtonClickHandler implements View.OnClickListener
+  {
+
+    @Override
+    public void onClick(View v)
+    {
+      Intent intent = new Intent(WelcomeActivity.this, SignUpActivity.class);
+
+      startActivity(intent);
+    }
   }
 }
