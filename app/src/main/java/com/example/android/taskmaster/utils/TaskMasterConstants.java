c8b4d39becc34e17bb95e87381d03909a5af8ae8
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-11-29 22:49:09 -0600

    Added a default background color field.

--- app/src/main/java/com/example/android/taskmaster/utils/TaskMasterConstants.java
+++ app/src/main/java/com/example/android/taskmaster/utils/TaskMasterConstants.java
@@ -6,6 +6,8 @@ public class TaskMasterConstants
   public static final int GREEN_BACKGROUND = 2;
   public static final int INDIGO_BACKGROUND = 3;
 
+  public static final int DEFAULT_BACKGROUND = TaskMasterConstants.INDIGO_BACKGROUND;
+
   private TaskMasterConstants()
   {
 
