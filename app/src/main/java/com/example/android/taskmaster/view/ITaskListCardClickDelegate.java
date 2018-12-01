ae27e83b7baa231a93b9cf9688127fb6767d0425
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-11-30 21:53:37 -0600

    Initial commit.

---
+++ app/src/main/java/com/example/android/taskmaster/view/ITaskListCardClickDelegate.java
@@ -0,0 +1,8 @@
+package com.example.android.taskmaster.view;
+
+import com.example.android.taskmaster.model.TaskListCardModel;
+
+public interface ITaskListCardClickDelegate
+{
+  public void onTaskListCardClick(TaskListCardModel taskListCardModel);
+}
