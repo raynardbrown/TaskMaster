a886652d3fbfd06a38c2ce4a5b117a7342b8c82c
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-12-19 04:08:57 -0600

    Added date/time constants that act as indexes for string array resources.
    Added an activity result code for the image picker.
    
    Added constants that identify an attachment type.

--- app/src/main/java/com/example/android/taskmaster/utils/TaskMasterConstants.java
+++ app/src/main/java/com/example/android/taskmaster/utils/TaskMasterConstants.java
@@ -8,6 +8,18 @@ public class TaskMasterConstants
 
   public static final int DEFAULT_BACKGROUND = TaskMasterConstants.INDIGO_BACKGROUND;
 
+  public static final int DUE_DATE_DATE_TODAY = 0;
+  public static final int DUE_DATE_DATE_TOMORROW = 1;
+
+  public static final int DUE_DATE_TIME_MORNING = 0;
+  public static final int DUE_DATE_TIME_NOON = 1;
+  public static final int DUE_DATE_TIME_NIGHT = 2;
+
+  public static final int ATTACHMENT_TYPE_TEXT_URL = 1;
+  public static final int ATTACHMENT_TYPE_IMAGE = 2;
+
+  public static final int IMAGE_REQUEST_CODE = 2;
+
   private TaskMasterConstants()
   {
 
