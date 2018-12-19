8ade8ad340ffd2a8ce59ad2ec6b8a69c1ce49005
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-12-18 22:41:05 -0600

    Updated the parameter list to properly handle a task list move.

--- app/src/main/java/com/example/android/taskmaster/view/dialog/IMoveTaskListDialogListener.java
+++ app/src/main/java/com/example/android/taskmaster/view/dialog/IMoveTaskListDialogListener.java
@@ -2,5 +2,18 @@ package com.example.android.taskmaster.view.dialog;
 
 public interface IMoveTaskListDialogListener
 {
-  public void onTaskListMoveClick(String newTaskGroup);
+  /**
+   * Triggered when the user confirms that they want to move the specified task list to another
+   * task group.
+   *
+   * @param taskListIndex the index of the task list within the current task group
+   *
+   * @param currentTaskGroupIndex the index of the current task group
+   *
+   * @param newTaskGroupIndex the index of the task group where the specified task list shall be
+   *                          moved.
+   */
+  public void onTaskListMoveClick(int taskListIndex,
+                                  int currentTaskGroupIndex,
+                                  int newTaskGroupIndex);
 }
