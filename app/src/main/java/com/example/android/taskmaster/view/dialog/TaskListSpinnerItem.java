8b959024b806eb4481bd7170b47cd7af33e4ce52
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-12-18 20:08:28 -0600

    Implement parcelable.

--- app/src/main/java/com/example/android/taskmaster/view/dialog/TaskListSpinnerItem.java
+++ app/src/main/java/com/example/android/taskmaster/view/dialog/TaskListSpinnerItem.java
@@ -1,6 +1,9 @@
 package com.example.android.taskmaster.view.dialog;
 
-public class TaskListSpinnerItem
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TaskListSpinnerItem implements Parcelable
 {
   private String taskListName;
 
@@ -15,6 +18,27 @@ public class TaskListSpinnerItem
     this.taskListName = taskListName;
     this.color = color;
   }
+
+  protected TaskListSpinnerItem(Parcel in)
+  {
+    taskListName = in.readString();
+    color = in.readInt();
+  }
+
+  public static final Creator<TaskListSpinnerItem> CREATOR = new Creator<TaskListSpinnerItem>()
+  {
+    @Override
+    public TaskListSpinnerItem createFromParcel(Parcel in)
+    {
+      return new TaskListSpinnerItem(in);
+    }
+
+    @Override
+    public TaskListSpinnerItem[] newArray(int size)
+    {
+      return new TaskListSpinnerItem[size];
+    }
+  };
 
   public String getTaskListName()
   {
@@ -34,5 +58,18 @@ public class TaskListSpinnerItem
   public void setColor(int color)
   {
     this.color = color;
+  }
+
+  @Override
+  public int describeContents()
+  {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags)
+  {
+    dest.writeString(taskListName);
+    dest.writeInt(color);
   }
 }
