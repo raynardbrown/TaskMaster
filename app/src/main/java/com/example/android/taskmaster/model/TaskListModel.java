6458b7034a889fb3b81896189c96ced7ce8589b2
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-12-18 19:40:45 -0600

    Removed the adapter and child list in order to have a "clean" model.

--- app/src/main/java/com/example/android/taskmaster/model/TaskListModel.java
+++ app/src/main/java/com/example/android/taskmaster/model/TaskListModel.java
@@ -1,30 +1,22 @@
 package com.example.android.taskmaster.model;
 
-import com.example.android.taskmaster.view.TaskListListAdapter;
-
-import java.util.List;
-
 public class TaskListModel
 {
   private final String taskGroupId;
   private final String taskListId;
   private final int taskIndex;
   private final String title;
-  private List<TaskListCardModel> cardList;
-  private TaskListListAdapter adapter;
+
 
   public TaskListModel(String taskGroupId,
                        String taskListId,
                        int taskIndex,
-                       String title,
-                       List<TaskListCardModel> cardList)
+                       String title)
   {
     this.taskGroupId = taskGroupId;
     this.taskListId = taskListId;
     this.taskIndex = taskIndex;
     this.title = title;
-    this.cardList = cardList;
-    this.adapter = adapter;
   }
 
   public String getTaskGroupId()
@@ -45,25 +37,5 @@ public class TaskListModel
   public String getTitle()
   {
     return title;
-  }
-
-  public List<TaskListCardModel> getCardList()
-  {
-    return cardList;
-  }
-
-  public void setCardList(List<TaskListCardModel> cardList)
-  {
-    this.cardList = cardList;
-  }
-
-  public TaskListListAdapter getAdapter()
-  {
-    return adapter;
-  }
-
-  public void setAdapter(TaskListListAdapter adapter)
-  {
-    this.adapter = adapter;
   }
 }
