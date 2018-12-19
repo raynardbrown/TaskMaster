11c82f77fdc10fea7230ee08c917fbd154430865
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-12-19 04:10:23 -0600

    Added several helper functions.

--- app/src/main/java/com/example/android/taskmaster/utils/TaskMasterUtils.java
+++ app/src/main/java/com/example/android/taskmaster/utils/TaskMasterUtils.java
@@ -1,6 +1,15 @@
 package com.example.android.taskmaster.utils;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
 
 import com.example.android.taskmaster.R;
 
@@ -29,6 +38,75 @@ public class TaskMasterUtils
       {
         return context.getResources().getColor(R.color.task_indigo);
       }
+    }
+  }
+
+  /**
+   * Remove the underline from the specified edit text returning the underline. Clients should
+   * cache the drawable in the event that the underline needs to be restored.
+   *
+   * @param editText the EditText whose underline shall be removed.
+   *
+   * @return the underline of the specified EditText as a Drawable.
+   *
+   * @see TaskMasterUtils#restoreEditTextUnderline(EditText, Drawable)
+   */
+  public static Drawable removeEditTextUnderline(EditText editText)
+  {
+    Drawable editTextDetailDescriptionBackgroundSave = TaskMasterUtils.retrieveEditTextUnderline(editText);
+
+    editText.setBackgroundResource(android.R.color.transparent);
+
+    return editTextDetailDescriptionBackgroundSave;
+  }
+
+  /**
+   * Restore the underline of the specified edit text using the specified drawable.
+   *
+   * @param editText the edit text whose underline shall be restored.
+   *
+   * @param underline the underline that shall be restore to the specified edit text.
+   *
+   * @see TaskMasterUtils#removeEditTextUnderline(EditText)
+   */
+  public static void restoreEditTextUnderline(EditText editText, Drawable underline)
+  {
+    editText.setBackground(underline);
+  }
+
+  public static Drawable retrieveEditTextUnderline(EditText editText)
+  {
+    return editText.getBackground();
+  }
+
+  public static Drawable setDrawableResColorRes(Context context,
+                                                @DrawableRes int resId,
+                                                @ColorRes int colorResId)
+  {
+    Drawable drawable = ContextCompat.getDrawable(context, resId);
+    drawable = DrawableCompat.wrap(drawable);
+    DrawableCompat.setTint(drawable, context.getResources().getColor(colorResId));
+
+    return drawable;
+  }
+
+  public static Drawable setDrawableResColor(Context context,
+                                             @DrawableRes int resId,
+                                             int colorValue)
+  {
+    Drawable drawable = ContextCompat.getDrawable(context, resId);
+    drawable = DrawableCompat.wrap(drawable);
+    DrawableCompat.setTint(drawable, colorValue);
+
+    return drawable;
+  }
+
+  public static void setStatusBarColorHelper(Window window, int newColor)
+  {
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+    {
+      window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+      window.setStatusBarColor(newColor);
     }
   }
 }
