81bf602f4f3f61bdbd650f7ae905ba3deb2b00e9
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-12-18 22:45:33 -0600

    Initial commit.

---
+++ app/src/main/java/com/example/android/taskmaster/view/dialog/IChooseAttachmentDialogListener.java
@@ -0,0 +1,9 @@
+package com.example.android.taskmaster.view.dialog;
+
+public interface IChooseAttachmentDialogListener
+{
+  public void onNewAttachment(String attachmentPath,
+                              int attachmentType,
+                              String attachmentExtraPath,
+                              int attachmentExtraType);
+}
