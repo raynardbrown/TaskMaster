2088ba56bb0aad933b046e848303b4c6a0c28f4b
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-12-19 18:57:08 -0600

    Use a custom positive button click handler so that we can validate user input. The default onClick function dismisses the dialog when clicked so we are unable to perform validation and warn the user prior to closing the dialog.
    Make sure the send password button is disabled if there is no user
    input.

--- app/src/main/java/com/example/android/taskmaster/view/dialog/ForgotPasswordDialogFragment.java
+++ app/src/main/java/com/example/android/taskmaster/view/dialog/ForgotPasswordDialogFragment.java
@@ -8,10 +8,16 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.android.taskmaster.R;
 import com.example.android.taskmaster.databinding.DialogSendPasswordBinding;
+import com.example.android.taskmaster.utils.InputValidator;
 
 public class ForgotPasswordDialogFragment extends DialogFragment
 {
@@ -49,7 +55,7 @@ public class ForgotPasswordDialogFragment extends DialogFragment
               @Override
               public void onClick(DialogInterface dialog, int which)
               {
-                listener.onSendPasswordClick(binding.etDialogEmailAddress.getText().toString());
+                // unused, we are using a custom click handler so that we can validate user input.
               }
             })
             .setNegativeButton(R.string.log_in_activity_dialog_send_password_cancel_button_string, new DialogInterface.OnClickListener()
@@ -61,6 +67,106 @@ public class ForgotPasswordDialogFragment extends DialogFragment
               }
             });
 
-    return builder.create();
+    final AlertDialog alertDialog = builder.create();
+
+    alertDialog.setOnShowListener(new DialogInterface.OnShowListener()
+    {
+      @Override
+      public void onShow(DialogInterface dialog)
+      {
+        // Initially disable the send password button
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+
+        // add a custom click handler to the send password button so that we can verify that
+        // the user entered a valid password. The default click handler for the dialog will just
+        // dismiss the dialog after the positive button has been pressed.
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new SendPasswordClickHandler(alertDialog));
+      }
+    });
+
+    setupTextChangeListener(alertDialog);
+
+    return alertDialog;
+  }
+
+  private void setupTextChangeListener(AlertDialog dialog)
+  {
+    binding.etDialogEmailAddress.addTextChangedListener(new EmailAddressTextWatcher(dialog));
+  }
+
+  class EmailAddressTextWatcher implements TextWatcher
+  {
+    private AlertDialog alertDialog;
+
+    EmailAddressTextWatcher(AlertDialog alertDialog)
+    {
+      this.alertDialog = alertDialog;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after)
+    {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count)
+    {
+      if(TextUtils.isEmpty(s))
+      {
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+      }
+      else
+      {
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+      }
+    }
+
+    @Override
+    public void afterTextChanged(Editable s)
+    {
+
+    }
+  }
+
+  class SendPasswordClickHandler implements View.OnClickListener
+  {
+    private AlertDialog alertDialog;
+
+    SendPasswordClickHandler(AlertDialog alertDialog)
+    {
+      this.alertDialog = alertDialog;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+      // Validate the email email address
+
+      EditText emailAddressEditText = binding.etDialogEmailAddress;
+      String emailString = emailAddressEditText.getText().toString();
+
+      emailString = InputValidator.sanitizeInput(emailString);
+
+      if(InputValidator.isValidInput(emailString))
+      {
+        if(InputValidator.isEmailValid(emailString))
+        {
+          emailAddressEditText.setError(null);
+
+          listener.onSendPasswordClick(emailString);
+
+          alertDialog.dismiss();
+        }
+        else
+        {
+          emailAddressEditText.setError(getString(R.string.error_log_in_activity_dialog_send_password_email_address_string));
+        }
+      }
+      else
+      {
+        emailAddressEditText.setError(getString(R.string.error_log_in_activity_dialog_send_password_email_address_string));
+      }
+    }
   }
 }
