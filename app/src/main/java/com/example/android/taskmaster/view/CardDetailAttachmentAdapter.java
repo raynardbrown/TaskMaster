99efa972852fca2a8ba4da6dc3e0813d8c549a19
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-12-27 01:04:11 -0600

    Added click handlers to the attachment views that display a pop up menu.
    Use a scaled down image attachment if an image is too large.
    
    Handle loading the image attachment data from the model. This will
    change since image data can be too large to pass around the Android
    framework.

--- app/src/main/java/com/example/android/taskmaster/view/CardDetailAttachmentAdapter.java
+++ app/src/main/java/com/example/android/taskmaster/view/CardDetailAttachmentAdapter.java
@@ -3,13 +3,20 @@ package com.example.android.taskmaster.view;
 import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Base64;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,6 +27,7 @@ import com.example.android.taskmaster.utils.TaskMasterConstants;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.text.Format;
 import java.text.SimpleDateFormat;
@@ -70,17 +78,52 @@ public class CardDetailAttachmentAdapter extends RecyclerView.Adapter<CardDetail
       // bind the image layout data, we need to check if the data is empty and if it is grab the data
       // from the attachment path, otherwise just use the data
 
-      // load the image
-      final ImageView imageView = itemBinding.ivAttachmentImageType;
+      String attachmentData = attachmentCreationInfo.getAttachmentModel().getAttachmentData();
 
-      Picasso.with(imageView.getContext())
-              .load(attachmentCreationInfo.getAttachmentPath())
-              .into(imageView, new AttachmentLoadedCallback(holder, attachmentCreationInfo));
+      if(attachmentData.isEmpty())
+      {
+        // load the image
+        final ImageView imageView = itemBinding.ivAttachmentImageType;
+
+        WindowManager windowManager = (WindowManager)imageView.getContext().getSystemService(Context.WINDOW_SERVICE);
+
+        if(windowManager != null)
+        {
+          Display display = windowManager.getDefaultDisplay();
+          DisplayMetrics outMetrics = new DisplayMetrics();
+          display.getMetrics(outMetrics);
+
+          float density = imageView.getResources().getDisplayMetrics().density;
+          float dpWidth = outMetrics.widthPixels / density;
+
+          Picasso.with(imageView.getContext())
+                  .load(attachmentCreationInfo.getAttachmentPath())
+                  .resize(dpToPixel(imageView.getContext(), dpWidth), dpToPixel(imageView.getContext(),
+                          300)) // TODO: Don't use this hardcoded value, add it to dimens.xml and pull it from there.
+                  .onlyScaleDown()
+                  .into(imageView, new AttachmentLoadedCallback(holder, attachmentCreationInfo));
+        }
+      }
+      else
+      {
+        // TODO: fetch the attachment data from the database, DO NOT USE ATTACHMENT DATA!!! The attachment data could be too large to send throughout the app (i.e. intents)
+        byte[] base64AttachmentData = Base64.decode(attachmentData,
+                Base64.NO_WRAP);
+
+        final ImageView imageView = itemBinding.ivAttachmentImageType;
+
+        Bitmap bitmap = BitmapFactory.decodeStream(new ByteArrayInputStream(base64AttachmentData));
+
+        imageView.setImageBitmap(bitmap);
+
+        attachmentListener.onImageAttachmentLoaded(holder.getAdapterPosition(), imageView);
+      }
 
       // fill in the time ui
       fillInTimeUi(itemBinding.tvAttachmentImageTypeTime, attachmentCreationInfo);
 
-      // TODO: set up menu click listener
+      // set up menu click listener
+      itemBinding.menuButtonAttachmentImageType.setOnClickListener(new AttachmentImageMenuClickListener(holder));
     }
     else if(attachmentCreationInfo.getAttachmentType() == TaskMasterConstants.ATTACHMENT_TYPE_TEXT_URL)
     {
@@ -115,10 +158,19 @@ public class CardDetailAttachmentAdapter extends RecyclerView.Adapter<CardDetail
         attachmentCreationInfo.getAttachmentExtraDataModel().setAttachmentExtraData(base64AttachmentLinkExtraData);
       }
 
-      // TODO: Notify the card activity so that the database can be updated
+      // set up menu click listener
+      itemBinding.menuButtonAttachmentLinkType.setOnClickListener(new AttachmentLinkMenuClickListener(holder));
 
-      // TODO: set up menu click listener
+      // TODO: Notify the card activity so that the database can be updated
     }
+  }
+
+  // TODO: Move this to utils
+  public int dpToPixel(Context context, float dp)
+  {
+    float density = context.getResources().getDisplayMetrics().density;
+
+    return (int)(dp * density);
   }
 
   @Override
@@ -131,10 +183,10 @@ public class CardDetailAttachmentAdapter extends RecyclerView.Adapter<CardDetail
   {
     Context context = textView.getContext();
 
-    Format dateFormatter = new SimpleDateFormat(context.getString(R.string.date_time_format_day_of_week_month_day_year_string));
+    Format dateFormatter = new SimpleDateFormat(context.getString(R.string.date_time_format_day_of_week_full_month_name_day_year_string));
     String dateFormatted = dateFormatter.format(attachmentCreationInfo.getAttachmentModel().getAttachmentTime());
 
-    Format timeFormatter = new SimpleDateFormat(context.getString(R.string.date_time_format_hour_minute_period_string));
+    Format timeFormatter = new SimpleDateFormat(context.getString(R.string.date_time_format_hour_with_leading_zero_minute_period_string));
     String timeFormatted = timeFormatter.format(attachmentCreationInfo.getAttachmentModel().getAttachmentTime());
 
     String dateTimeString = String.format(textView.getResources().getString(R.string.card_detail_activity_attachment_time_format_string),
@@ -183,6 +235,129 @@ public class CardDetailAttachmentAdapter extends RecyclerView.Adapter<CardDetail
     public void onError()
     {
       // TODO: Handle error, maybe putting in a placeholder
+    }
+  }
+
+  class AttachmentLinkMenuClickListener implements View.OnClickListener
+  {
+    private CardDetailAttachmentAdapterViewHolder holder;
+
+    AttachmentLinkMenuClickListener(CardDetailAttachmentAdapterViewHolder holder)
+    {
+      this.holder = holder;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+      PopupMenu popup = new PopupMenu(v.getContext(), v);
+      popup.setOnMenuItemClickListener(new AttachmentLinkMenuItemClickListener(holder));
+      MenuInflater inflater = popup.getMenuInflater();
+      inflater.inflate(R.menu.menu_attachment_link, popup.getMenu());
+      popup.show();
+    }
+  }
+
+  class AttachmentLinkMenuItemClickListener implements PopupMenu.OnMenuItemClickListener
+  {
+    private CardDetailAttachmentAdapterViewHolder holder;
+
+    AttachmentLinkMenuItemClickListener(CardDetailAttachmentAdapterViewHolder holder)
+    {
+      this.holder = holder;
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item)
+    {
+      switch(item.getItemId())
+      {
+        case R.id.action_delete_link_attachment:
+        {
+          attachmentListener.onAttachmentRemoveRequest(holder.getAdapterPosition());
+
+          return true;
+        }
+
+        default:
+        {
+          return false;
+        }
+      }
+    }
+  }
+
+  class AttachmentImageMenuClickListener implements View.OnClickListener
+  {
+    private CardDetailAttachmentAdapterViewHolder holder;
+
+    AttachmentImageMenuClickListener(CardDetailAttachmentAdapterViewHolder holder)
+    {
+      this.holder = holder;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+      PopupMenu popup = new PopupMenu(v.getContext(), v);
+      popup.setOnMenuItemClickListener(new AttachmentImageMenuItemClickListener(holder));
+      MenuInflater inflater = popup.getMenuInflater();
+      inflater.inflate(R.menu.menu_attachment_image, popup.getMenu());
+
+      if(attachmentList.get(holder.getAdapterPosition()).getAttachmentModel().isBound())
+      {
+        popup.getMenu().findItem(R.id.action_remove_attachment_image_from_toolbar).setVisible(true);
+        popup.getMenu().findItem(R.id.action_add_attachment_image_to_toolbar).setVisible(false);
+      }
+      else
+      {
+        popup.getMenu().findItem(R.id.action_add_attachment_image_to_toolbar).setVisible(true);
+        popup.getMenu().findItem(R.id.action_remove_attachment_image_from_toolbar).setVisible(false);
+      }
+
+      popup.show();
+    }
+  }
+
+  class AttachmentImageMenuItemClickListener implements PopupMenu.OnMenuItemClickListener
+  {
+    private CardDetailAttachmentAdapterViewHolder holder;
+
+    AttachmentImageMenuItemClickListener(CardDetailAttachmentAdapterViewHolder holder)
+    {
+      this.holder = holder;
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item)
+    {
+      switch(item.getItemId())
+      {
+        case R.id.action_delete_attachment_image:
+        {
+          attachmentListener.onAttachmentRemoveRequest(holder.getAdapterPosition());
+
+          return true;
+        }
+
+        case R.id.action_add_attachment_image_to_toolbar:
+        {
+          attachmentListener.onAttachmentBindRequest(holder.getAdapterPosition(),
+                  holder.itemBinding.ivAttachmentImageType);
+          return true;
+        }
+
+        case R.id.action_remove_attachment_image_from_toolbar:
+        {
+          attachmentListener.onAttachmentUnbindRequest(holder.getAdapterPosition());
+          return true;
+        }
+
+        default:
+        {
+          return false;
+        }
+      }
     }
   }
 
