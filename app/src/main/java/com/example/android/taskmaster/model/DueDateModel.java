a5dedc60313c60cb73aa8fc5ec39d4c992103297
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-12-27 00:38:43 -0600

    Implemented Parcelable.

--- app/src/main/java/com/example/android/taskmaster/model/DueDateModel.java
+++ app/src/main/java/com/example/android/taskmaster/model/DueDateModel.java
@@ -1,6 +1,9 @@
 package com.example.android.taskmaster.model;
 
-public class DueDateModel
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class DueDateModel implements Parcelable
 {
   private String cardId;
   private long dueDate;
@@ -12,6 +15,28 @@ public class DueDateModel
     this.dueDate = dueDate;
     this.completed = completed;
   }
+
+  protected DueDateModel(Parcel in)
+  {
+    cardId = in.readString();
+    dueDate = in.readLong();
+    completed = in.readByte() != 0;
+  }
+
+  public static final Creator<DueDateModel> CREATOR = new Creator<DueDateModel>()
+  {
+    @Override
+    public DueDateModel createFromParcel(Parcel in)
+    {
+      return new DueDateModel(in);
+    }
+
+    @Override
+    public DueDateModel[] newArray(int size)
+    {
+      return new DueDateModel[size];
+    }
+  };
 
   public String getCardId()
   {
@@ -41,5 +66,19 @@ public class DueDateModel
   public void setCompleted(boolean completed)
   {
     this.completed = completed;
+  }
+
+  @Override
+  public int describeContents()
+  {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags)
+  {
+    dest.writeString(cardId);
+    dest.writeLong(dueDate);
+    dest.writeByte((byte) (completed ? 1 : 0));
   }
 }
