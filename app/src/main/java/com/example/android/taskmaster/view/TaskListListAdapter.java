6ac03bcb72f8dbebd0e7b840b62cafffe7b27cbc
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-01-05 02:18:29 -0600

    Added a constructor parameter that takes in a list that contains a due date associated with each card.
    Display a due date icon and text for each card that has a due date that
    has not been completed.
    
    Added a helper function that sets the correct color for the due date
    icon and text.

--- app/src/main/java/com/example/android/taskmaster/view/TaskListListAdapter.java
+++ app/src/main/java/com/example/android/taskmaster/view/TaskListListAdapter.java
@@ -2,17 +2,26 @@ package com.example.android.taskmaster.view;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.android.taskmaster.R;
 import com.example.android.taskmaster.databinding.TaskMasterTaskListItemBinding;
+import com.example.android.taskmaster.model.DueDateModel;
 import com.example.android.taskmaster.model.TaskListCardModel;
+import com.example.android.taskmaster.utils.TaskMasterUtils;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -31,6 +40,7 @@ public class TaskListListAdapter extends RecyclerView.Adapter<TaskListListAdapte
    * The position within the list of the card that is currently being dragged.
    */
   private int draggedCardPosition;
+  private List<DueDateModel> dueDateModelList;
 
   /**
    * Create a new TaskListListAdapter.
@@ -49,13 +59,17 @@ public class TaskListListAdapter extends RecyclerView.Adapter<TaskListListAdapte
    *
    * @param draggedCardPosition the position within the list of the card being dragged or -1 if
    *                            there is no card being dragged.
+   *
+   * @param dueDateModelList the list of due dates associated with each card. An entry in this list
+   *                         could be null if the card a the same index does not have a due date.
    */
   TaskListListAdapter(List<TaskListCardModel> taskListItemList,
                       ITaskListCardClickDelegate taskListCardClickDelegate,
                       String taskListTitle,
                       TaskListAdapter taskListAdapter,
                       int taskListAdapterPosition,
-                      int draggedCardPosition)
+                      int draggedCardPosition,
+                      List<DueDateModel> dueDateModelList)
   {
     this.taskListItemList = taskListItemList;
     this.taskListCardClickDelegate = taskListCardClickDelegate;
@@ -63,6 +77,7 @@ public class TaskListListAdapter extends RecyclerView.Adapter<TaskListListAdapte
     this.taskListAdapter = taskListAdapter;
     this.taskListAdapterPosition = taskListAdapterPosition;
     this.draggedCardPosition = draggedCardPosition;
+    this.dueDateModelList = dueDateModelList;
   }
 
   @Override
@@ -123,7 +138,27 @@ public class TaskListListAdapter extends RecyclerView.Adapter<TaskListListAdapte
 
     // TODO: Grab the checklist count from the database, display the image view and completion ratio accordingly
 
-    // TODO: Grab the due date from the database, display the image view and date accordingly (do not display completed due dates) set colors too
+    // Grab the due date from the database, display the image view and date accordingly
+    // (do not display completed due dates) set colors too
+    DueDateModel dueDateModel = dueDateModelList.get(position);
+
+    if(dueDateModel != null && !dueDateModel.isCompleted())
+    {
+      itemBinding.ivTaskListItemDueDate.setVisibility(View.VISIBLE);
+      itemBinding.tvTaskListItemDueDate.setVisibility(View.VISIBLE);
+
+      initDueDateUi(itemBinding.ivTaskListItemDueDate.getContext(),
+              new Date(dueDateModel.getDueDate()),
+              itemBinding.ivTaskListItemDueDate,
+              itemBinding.tvTaskListItemDueDate);
+
+      anyIconsInRow = true;
+    }
+    else
+    {
+      itemBinding.ivTaskListItemDueDate.setVisibility(View.GONE);
+      itemBinding.tvTaskListItemDueDate.setVisibility(View.GONE);
+    }
 
     if(anyIconsInRow)
     {
@@ -145,6 +180,93 @@ public class TaskListListAdapter extends RecyclerView.Adapter<TaskListListAdapte
   public List<TaskListCardModel> getList()
   {
     return taskListItemList;
+  }
+
+  private void initDueDateUi(Context context,
+                             Date date,
+                             ImageView dueDateImageView,
+                             TextView dueDateTextView)
+  {
+    // Build the string
+    Calendar dueDateAsCalendarObject = Calendar.getInstance();
+    dueDateAsCalendarObject.setTime(date);
+
+    // Set up a comparison of the current time against the due date
+    int compareDate = Calendar.getInstance().compareTo(dueDateAsCalendarObject);
+
+    // Check the current time against the due date and set the icon color
+    Drawable drawable;
+
+    if(compareDate <= 0)
+    {
+      // Not due yet
+
+      // determine how close we are to the due date
+      Calendar calendar24HoursFromNow = Calendar.getInstance();
+      calendar24HoursFromNow.add(Calendar.HOUR_OF_DAY, 24);
+
+      int compareTime = calendar24HoursFromNow.compareTo(dueDateAsCalendarObject);
+
+      if(compareTime <= 0)
+      {
+        // we have at least 24 hours left
+        drawable = TaskMasterUtils.setDrawableResColorRes(context,
+                R.drawable.ic_baseline_schedule_24px,
+                R.color.due_date_soon);
+
+        Format dateFormatter = new SimpleDateFormat(dueDateImageView.getResources().getString(R.string.date_time_format_abbreviated_month_name_day_string));
+        String dateFormatted = dateFormatter.format(dueDateAsCalendarObject.getTime());
+
+        dueDateTextView.setText(dateFormatted);
+        dueDateTextView.setTextColor(dueDateTextView.getResources().getColor(R.color.due_date_soon));
+      }
+      else
+      {
+        // we have less than 24 hours left
+        drawable = TaskMasterUtils.setDrawableResColorRes(context,
+                R.drawable.ic_baseline_schedule_24px,
+                R.color.due_date_sooner);
+
+        Format timeFormatter = new SimpleDateFormat(dueDateImageView.getResources().getString(R.string.date_time_format_hour_with_leading_zero_minute_period_string));
+        String timeFormatted = timeFormatter.format(dueDateAsCalendarObject.getTime());
+
+        dueDateTextView.setText(timeFormatted);
+        dueDateTextView.setTextColor(dueDateTextView.getResources().getColor(R.color.due_date_sooner));
+      }
+    }
+    else
+    {
+      // past due
+
+      // set the past due color
+      drawable = TaskMasterUtils.setDrawableResColorRes(context,
+              R.drawable.ic_baseline_schedule_24px,
+              R.color.due_date_past);
+
+      // See how far past the due we are, since we may need to add the year
+      int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+      int dueDateYear = dueDateAsCalendarObject.get(Calendar.YEAR);
+
+      Format dateFormatter;
+      String dateFormatted;
+      if(currentYear == dueDateYear)
+      {
+        // current year, so no need to show the year
+        dateFormatter = new SimpleDateFormat(dueDateImageView.getResources().getString(R.string.date_time_format_abbreviated_month_name_day_string));
+        dateFormatted = dateFormatter.format(dueDateAsCalendarObject.getTime());
+      }
+      else
+      {
+        // not the current year, so we need to show the year
+        dateFormatter = new SimpleDateFormat(dueDateImageView.getResources().getString(R.string.date_time_format_abbreviated_month_name_day_year_string));
+        dateFormatted = dateFormatter.format(dueDateAsCalendarObject.getTime());
+      }
+
+      dueDateTextView.setText(dateFormatted);
+      dueDateTextView.setTextColor(dueDateTextView.getResources().getColor(R.color.due_date_past));
+    }
+
+    dueDateImageView.setImageDrawable(drawable);
   }
 
   /**
