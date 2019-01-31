45a32ba505b63a4bb196fa8b035796d1c5cb4b77
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-01-31 04:04:41 -0600

    Added a simple slide transition between this activity and the log in and sign up activity.

--- app/src/main/java/com/example/android/taskmaster/view/WelcomeActivity.java
+++ app/src/main/java/com/example/android/taskmaster/view/WelcomeActivity.java
@@ -1,11 +1,16 @@
 package com.example.android.taskmaster.view;
 
+import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
 
 import com.example.android.taskmaster.R;
 import com.example.android.taskmaster.databinding.ActivityWelcomeBinding;
@@ -30,6 +35,16 @@ public class WelcomeActivity extends AppCompatActivity
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
+
+    // must be called before set content view
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+    {
+      getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+
+      // set a slide exit transition
+      getWindow().setExitTransition(new Slide(Gravity.END));
+    }
+
     binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome);
 
     registerClickHandlers();
@@ -48,7 +63,15 @@ public class WelcomeActivity extends AppCompatActivity
     {
       Intent intent = LogInActivity.getStartIntent(WelcomeActivity.this);
 
-      startActivity(intent);
+      if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+      {
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(WelcomeActivity.this).toBundle());
+      }
+      else
+      {
+        // standard transition on older Android versions
+        startActivity(intent);
+      }
     }
   }
 
@@ -59,7 +82,15 @@ public class WelcomeActivity extends AppCompatActivity
     {
       Intent intent = SignUpActivity.getStartIntent(WelcomeActivity.this);
 
-      startActivity(intent);
+      if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+      {
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(WelcomeActivity.this).toBundle());
+      }
+      else
+      {
+        // standard transition on older Android versions
+        startActivity(intent);
+      }
     }
   }
 }
